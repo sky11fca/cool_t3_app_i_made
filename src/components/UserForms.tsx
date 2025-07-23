f9559b82196d3f23401c44230dacb1eeec714fbc
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-07-23 10:28:54 +0300

    Finished the user adminspace more additions

--- src/components/UserForms.tsx
+++ src/components/UserForms.tsx
@@ -1,17 +1,90 @@
 
 import {api} from "~/trpc/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import Popup from "reactjs-popup";
+
+interface User {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function UserForms() {
-  const {data: allUsers} = api.signup.getUsers.useQuery();
+  const {data: allUsers, refetch} = api.signup.getUsers.useQuery();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  })
 
 
   const router = useRouter();
 
 
+  const addUserMutation = api.signup.register.useMutation({
+    onSuccess: () => {
+      void refetch();
+    },
+  });
+
+  const deleteUserMutation = api.signup.deleteUser.useMutation({
+    onSuccess: () => {
+      void refetch();
+    },
+  })
+
+
+  const handleAddUser = () => {
+    addUserMutation.mutate({
+      username: formData.username,
+      email: formData.email,
+      password: formData.password,
+    })
+  }
+
+  const handleDeleteUser = (user: User) => {
+    if(confirm("Are you sure?")){
+      deleteUserMutation.mutate(user)
+    }
+
+  }
+
   return (
     <div>
-      <button>Add user</button>
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
+
+        <button
+          onClick={() => {
+            handleAddUser();
+          }}
+        >
+          Add user
+        </button>
+      </form>
       <button onClick={() => router.push("/")}>GO BACK</button>
       <table>
         <thead>
@@ -23,16 +96,30 @@ export default function UserForms() {
           </tr>
         </thead>
         <tbody>
-        {allUsers?.map((user) => (
-          <tr key={user.id}>
-            <td>{user.id}</td>
-            <td>{user.username}</td>
-            <td>{user.email}</td>
-            <td>{user.password}</td>
-          </tr>
-        ))}
+          {allUsers?.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+              <td>
+                <button
+                  onClick={() => {handleDeleteUser(user as User);}}
+                >
+                  Delete
+                </button>
+              </td>
+              <td>
+                <Popup
+                  trigger={<button>Edit</button>}
+                  position={"top center"}>
+                  <p>This functionality is unavalable now</p>
+                </Popup>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
